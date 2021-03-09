fbbfeb22f1585cb7a1ae289d99df986ff814fac4
ahmed1amen <ahmed1amen.g@gmail.com>
2021-03-10 01:55:38 +0200

    init commit

---
+++ database/migrations/2021_03_09_224204_create_users_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('sub')->nullable();
+            $table->string('name')->nullable();
+            $table->string('given_name')->nullable();
+            $table->string('family_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->string('nickname')->nullable();
+            $table->string('preferred_username')->nullable();
+            $table->string('profile')->nullable();
+            $table->string('picture')->nullable();
+            $table->string('website')->nullable();
+            $table->string('email')->nullable();
+            $table->string('email_verified_at')->nullable();
+            $table->string('gender')->nullable();
+            $table->date('birthdate')->nullable();
+            $table->string('zoneinfo')->nullable();
+            $table->string('locale')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->timestamp('phone_number_verified_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
